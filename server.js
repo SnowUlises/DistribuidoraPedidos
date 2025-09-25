68fdd9757cc6fd4f25e4f62b87835b2d85921fd2
SnowUlises <snowulises1221@gmail.com>
2025-09-25 12:41:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import { createClient } from '@supabase/supabase-js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
@@ -443,6 +443,7 @@ app.delete('/api/eliminar-pedido/:id', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server escuchando en http://localhost:${PORT}`);
 });
+
 
 
 
